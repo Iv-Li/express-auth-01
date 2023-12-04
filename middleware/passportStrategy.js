99b614ec19ddd8c7b5394dded45751e8d2bd0bc1
Ivanna Liuba <i.ivanna.liuba@gmail.com>
2023-12-04 13:42:29 +0100

    add Google auth

--- middleware/passportStrategy.js
+++ middleware/passportStrategy.js
@@ -1,6 +1,7 @@
 const crypto = require('crypto')
 const db = require('../db/models')
 
+/* LOCAL STRATEGY */
 function verify(username, password, done) {
   db.get('SELECT * FROM users WHERE username = ?', [username], function (err, row){
     if(err) return done(err)
@@ -27,5 +28,47 @@ const deserializeUser = (user, done) => {
   })
 }
 
-module.exports = { verify, serializeUser, deserializeUser }
+/* GOOGLE STRATEGY */
+const googleVerify = (issuer, profile, done) => {
+  const setNewUserAndCredentials = (userName, provider, subject) => {
+    db.run('INSERT INTO users (name) VALUES (?)',
+      [userName],
+      function (err) {
+        if (err) done(err)
+
+        const id = this.lastID
+        db.run('INSERT INTO federated_credentials (user_id, provider, subject) VALUES (?, ?, ?)',
+          [id, provider, subject],
+          function (err) {
+            if(err) done(err)
+            const user = {
+              id,
+              name: profile.displayName
+            }
+            done(null, user)
+          })
+      })
+  }
+
+  const getExistedUser = (userId) => {
+    db.get('SELECT * FROM users WHERE id = ?', userId, function (err, row) {
+      if (err) done(err)
+      if (!row) done(null, false)
+      done(null, row)
+    })
+  }
+
+  db.get('SELECT * FROM federated_credentials WHERE provider = ? AND subject = ?',
+    [issuer, profile.id],
+    function (err, row) {
+      if(err) done(err)
+      if(!row) {
+        setNewUserAndCredentials(profile.displayName, issuer, profile.id)
+      } else {
+        getExistedUser(row.user_id)
+      }
+    })
+}
+
+module.exports = { verify, serializeUser, deserializeUser, googleVerify }
 
--- routes/auth.js
+++ routes/auth.js
@@ -2,16 +2,37 @@ const express = require('express')
 const router = express.Router()
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
+const GoogleStrategy = require('passport-google-oidc')
 
 const { login, renderSignUp, signUp, logout } = require('../controllers/auth')
-const { verify, serializeUser, deserializeUser } = require('../middleware/passportStrategy')
+const {
+  verify,
+  serializeUser,
+  deserializeUser,
+  googleVerify
+} = require('../middleware/passportStrategy')
 
 passport.use(new LocalStrategy(verify))
 passport.serializeUser(serializeUser)
 passport.deserializeUser(deserializeUser)
+const GoogleCallbackURL = '/oauth2/redirect/google'
+passport.use(new GoogleStrategy(
+  {
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: GoogleCallbackURL,
+    scope: ['profile']
+  },
+  googleVerify))
 
 router.get('/login', login)
 router.post('/login/password', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login'
+}))
+
+router.get('/login/federated/google', passport.authenticate('google'))
+router.get(GoogleCallbackURL, passport.authenticate('google', {
   successRedirect: '/',
   failureRedirect: '/login'
 }))
